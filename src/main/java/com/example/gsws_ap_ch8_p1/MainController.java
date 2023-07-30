ef2be3736fdf2ca793fb39a442d6ddf5f80ec043
Tony-Tor <Tony.Tor2016@yandex.ru>
2023-07-30 16:32:01 +0400

    ex3 Переменные пути

--- src/main/java/com/example/gsws_ap_ch8_p1/MainController.java
+++ src/main/java/com/example/gsws_ap_ch8_p1/MainController.java
@@ -2,14 +2,15 @@ package com.example.gsws_ap_ch8_p1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/home")
-    public String home(@RequestParam(required = false) String color,
+    @RequestMapping("/home/{color}")
+    public String home(@PathVariable String color,
             @RequestParam(required = false) String name, Model page){
         page.addAttribute("username", name);
         page.addAttribute("color", color);
